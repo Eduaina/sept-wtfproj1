1a45a633e4db8e3d6bc2e2214d4284dd432e1327
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-09-04 15:11:20 +0100

    Learned about variable types and how to use Arithmetic operators

---
+++ September/Week1-Assignment/script.js
@@ -0,0 +1,21 @@
+const num1 = 5;
+
+const num2 = 10;
+
+const num3 = 40;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+const divide = num3 / num2;
+console.log(divide);
+
+const add = num2 + num3
+console.log(add);
+
+const subtract = num3 - num1;
+console.log(subtract);
