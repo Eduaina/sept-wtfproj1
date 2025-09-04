a3bdab03679000021dc544ada2075299223da3c6
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-09-04 14:16:35 +0100

    Created  HTML and JS files

---
+++ September/javascript.js
@@ -0,0 +1,8 @@
+const legalAge = confirm("Are you above 18?")
+console.log(legalAge)
+
+const name = prompt("What is your name?");
+console.log(name);
+
+let jobTitle = prompt("What is your job role?");
+console.log(jobTitle);
