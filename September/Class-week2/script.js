d965f336156b68b419f6f550a7e16c522266036b
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-09-08 12:12:30 +0100

    Classwork for monday 8/9/25

--- September/Class-week2/script.js
+++ September/Class-week2/script.js
@@ -1,29 +1,54 @@
-const num1 = 30;
-const num2 = 10;
-const name = "Eduaina"
+// const num1 = 30;
+// const num2 = 10;
+// const name = "Eduaina"
 
 
-//Multiplication
-const multiply = num1 * num2
-console.log(multiply);
+// //Multiplication
+// const multiply = num1 * num2
+// console.log(multiply);
 
-//division
-var divide = num1 / num2;
-console.log(divide);
+// //division
+// var divide = num1 / num2;
+// console.log(divide);
 
-//Addition
-const add = num1 + num2
-console.log(add);
+// //Addition
+// const add = num1 + num2
+// console.log(add);
 
-const result = add;
-console.log(add);
+// const result = add;
+// console.log(add);
 
 
 
-//Hello, name your score is multiply
+// //Hello, name your score is multiply
 
-//string concatenation
-console.log("Hello, " + name + " your score is " + multiply);
+// //string concatenation
+// console.log("Hello, " + name + " your score is " + multiply);
 
-//template Literal
-console.log(`Hello, ${name} your score is ${multiply}`)
+// //template Literal
+// console.log(`Hello, ${name} your score is ${multiply}`)
+
+
+
+
+
+
+
+
+
+
+const monthlyIncome = 2000;
+console.log(monthlyIncome);
+
+let dailyExpense = monthlyIncome * 0.01;
+console.log(dailyExpense);
+
+let monthlyExpense = dailyExpense * 30;
+console.log(monthlyExpense);
+
+let savings = monthlyIncome - monthlyExpense;
+console.log(savings);
+
+//Remainder
+let spendings = monthlyExpense;
+console.log(spendings);
