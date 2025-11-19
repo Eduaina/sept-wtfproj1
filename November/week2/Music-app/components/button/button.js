bccec92bcaed78f4ae5c5f400363e87f39bd4078
Eduaina Ighalo <eduaighalo29@gmail.com>
2025-11-19 12:04:19 +0100

    19/11/25

---
+++ November/week2/Music-app/components/button/button.js
@@ -0,0 +1,3 @@
+document.getElementById("testBtn").addEventListener("click", function () {
+  alert("Button clicked!");
+});
---
+++ November/week2/practice/practice.js
@@ -0,0 +1,21 @@
+// const isEven = (num) => {
+//     if (num % 2 == 0) {
+//         return 'Even number';
+//     } else {
+//         return 'Odd number';
+//     }
+// }
+
+// console.log(isEven(3));
+// console.log(isEven(4));
+
+const isEven = (num) => {
+  if (num % 2 == 0) {
+    console.log("Even number");
+  } else {
+    console.log("Odd number");
+  }
+};
+
+isEven(0);
+isEven(4);
